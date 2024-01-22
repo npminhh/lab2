e3c010028072fad787009f248bad8157d50cf86b
npminhh <mink2kar4@gmail.com>
2024-01-22 12:01:43 +0700

    lab2

---
+++ code clg3/coding_clge3.js
@@ -0,0 +1,29 @@
+const gameEvents = new Map([
+   [17, '⚽ GOAL'],
+   [36, '🔃Substitution'],
+   [47, '⚽ GOAL'],
+   [61, '🔃Substitution'],
+   [64, '🟡 Yellow card'],
+   [69, '🔴 Red cord'],
+   [70, '🔃Substitution'],
+   [72, '🔃Substitution'],
+   [76, '⚽ GOAL'],
+   [80, '⚽ GOAL'],
+   [92, '🟡 Yellow card'],
+]);
+//1.
+// console.log(gameEvents.values());
+const events = new Set(gameEvents.values());
+console.log(events);
+//2.
+gameEvents.delete(64);
+//3.
+console.log(`An event happened, on average, every ${90 / gameEvents.size} minutes`);
+const time = [...gameEvents.keys()].pop();
+console.log(time);
+console.log(`An event happened, on average, every ${time / gameEvents.size} minutes`);
+//4.
+for(const[min, event] of gameEvents){
+    const half = min <=45 ? 'FIRST' : 'SECOND';
+    console.log(`[${half} HALF] ${min}:${event}`);
+}
